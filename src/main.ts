64d659d330b5f5597489ea2f869412aff4e31b50
Luca Schlecker <luca.schlecker@hotmail.com>
2025-01-31 17:47:40 +0100

    feat: allow configuration of port using environment variable

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,22 @@ import { snowballRDefinition } from "./grpc-gen/main.grpc-server";
 import { authInterceptor } from "./auth-interceptor";
 import { loggingInterceptor } from "./logging-interceptor";
 
+const port = process.env.GRPC_PORT ?? "8080";
+const address = process.env.GRPC_ADDRESS ?? "0.0.0.0";
+const endpoint = `${address}:${port}`;
+
 const server = new grpc.Server({
     interceptors: [authInterceptor, loggingInterceptor],
 });
 server.addService(snowballRDefinition, snowballRService);
 server.bindAsync(
-    "0.0.0.0:8081",
+    endpoint,
     grpc.ServerCredentials.createInsecure(),
     (err: Error | null, port: number) => {
         if (err) {
             console.error(`Server error: ${err.message}`);
         } else {
-            console.log(`Server bound on port: ${port}`);
+            console.log(`Server bound on: ${address}:${port}`);
         }
     },
 );
